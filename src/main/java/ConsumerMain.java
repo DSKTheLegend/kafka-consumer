5cbae45ec07f28166228566380f1b094a81ab460
Abhishek Raha <dsklegend@live.com>
2023-10-29 20:03:35 +0530

    added shutdown hook

--- src/main/java/ConsumerMain.java
+++ src/main/java/ConsumerMain.java
@@ -3,6 +3,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,18 +43,48 @@ public class ConsumerMain {
         // create consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 
-        // subscribe consumer to our topic(s)
-        consumer.subscribe(Arrays.asList(kafkaTopic));
+        // get a reference to the current thread
+        final Thread mainThread = Thread.currentThread();
 
-        // poll for new data
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+        // adding the shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Detected a shutdown, let's exit by calling consumer.wakeup()...");
+                consumer.wakeup();
 
-            for (ConsumerRecord<String, String> record : records) {
-                log.info("Key: " + record.key() + ", Value: " + record.value());
-                log.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+                // join the main thread to allow the execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        });
 
+        try {
+
+            // subscribe consumer to our topic(s)
+            consumer.subscribe(Arrays.asList(kafkaTopic));
+
+            // poll for new data
+            while (true) {
+                ConsumerRecords<String, String> records =
+                        consumer.poll(Duration.ofMillis(100));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key: " + record.key() + ", Value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+                }
+            }
+
+        } catch (WakeupException e) {
+            log.info("Wake up exception!");
+            // we ignore this as this is an expected exception when closing a consumer
+        } catch (Exception e) {
+            log.error("Unexpected exception", e);
+        } finally {
+            consumer.close(); // this will also commit the offsets if need be.
+            log.info("The consumer is now gracefully closed.");
+        }
     }
 }
